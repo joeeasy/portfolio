5d09c95f4f0b9fce53b95dd0121f09f91dd4e7a2
joeeasy <jehonadabokpus@gmail.com>
2018-03-04 12:59:05 +0100

    new projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,19 +18,20 @@ window.onload = function() {
             document.body.appendChild(css);
         },
         portfolio: [
-            {name: "Temitope Atte", siteUrl: "temitopeatte.org", imageUrl: 'http://res.cloudinary.com/dsxfchez8/image/upload/v1520163637/temitope-atte_gekpsg.png'},
-            
+            {name: "Teleworld Group", siteUrl: "teleworldgroup.com", imageUrl:  "http://res.cloudinary.com/dsxfchez8/image/upload/v1520163642/teleworldgroup_gly1dp.png"},
+            {name: "Lokal Broda", siteUrl: "lokalbroda.com", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1520163929/lokalbroda_uqmhp2.png" },
+            {name: "Keresuk Rice", siteUrl: "46.101.31.229", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1520163933/keresuk-ricepng_oxfves.png"},
+            {name: "Temitope Atte", siteUrl: "temitopeatte.org", imageUrl: 'http://res.cloudinary.com/dsxfchez8/image/upload/v1520163637/temitope-atte_gekpsg.png'},            
             {name: "Prime Agrolytics",siteUrl: "http://www.primeagrolytics.com/login", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1519746528/orimeagrolytics_hvjzps.png"},
-             {name: "Cards For Humanity", siteUrl: "https://cfh.io", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1519745242/screencapture-cfh-io-1519745125999_taa2tk.png"},
-             {name: "AJE",siteUrl: "https://joeeasy.github.io/aje/index.html", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847906/screencapture-joeeasy-github-io-aje-index-html-1510847847079_lpawda.png"},
+            {name: "Cards For Humanity", siteUrl: "https://cfh.io", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1519745242/screencapture-cfh-io-1519745125999_taa2tk.png"},
+            {name: "AJE",siteUrl: "https://joeeasy.github.io/aje/index.html", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847906/screencapture-joeeasy-github-io-aje-index-html-1510847847079_lpawda.png"},
+            {name: "Univelcity LMS", siteUrl:"https://univelcity-lms.herokuapp.com/", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510850324/screencapture-univelcity-lms-herokuapp-com-index-1510850269515_i8v6b5.png"},
              
-             {name: "Univelcity LMS", siteUrl:"http://res.cloudinary.com/dsxfchez8/image/upload/v1520163637/temitope-atte_gekpsg.png 'https://univelcity-lms.herokuapp.com/', imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510850324/screencapture-univelcity-lms-herokuapp-com-index-1510850269515_i8v6b5.png"},
+            {name: "Niger Delta Super Star", siteUrl: "http://nigerdeltasuperstar.org.ng", imageurl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510848302/screencapture-nigerdeltasuperstar-org-ng-1510848266383_ny219p.png"},
              
-             {name: "Niger Delta Super Star", siteUrl: "http://nigerdeltasuperstar.org.ng", imageurl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510848302/screencapture-nigerdeltasuperstar-org-ng-1510848266383_ny219p.png"},
+            {name: "Ikonfess", siteUrl: "http://www.ikonfess.com/", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847768/screencapture-www-ikonfess-com-1510847735636_hfrya9.png"},
              
-             {name: "Ikonfess", siteUrl: "http://www.ikonfess.com/", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847768/screencapture-www-ikonfess-com-1510847735636_hfrya9.png"},
-             
-             {name: "UK pubmed", siteUrl: "http://ukpubmed.org", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847524/ukpubmed_lo1j9y.png"},
+            {name: "UK pubmed", siteUrl: "http://ukpubmed.org", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847524/ukpubmed_lo1j9y.png"},
              
         ],
         loadPortfolio: function() {
