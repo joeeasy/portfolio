d6bae0ee7222e90875caaf2210bfcbdd8bb73aa4
joeeasy <jehonadabokpus@gmail.com>
2018-03-04 17:16:42 +0100

    removed two portfolio items

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,11 +26,6 @@ window.onload = function() {
             {name: "Cards For Humanity", siteUrl: "https://cfh.io", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1519745242/screencapture-cfh-io-1519745125999_taa2tk.png"},
             {name: "AJE",siteUrl: "https://joeeasy.github.io/aje/index.html", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847906/screencapture-joeeasy-github-io-aje-index-html-1510847847079_lpawda.png"},
             {name: "Univelcity LMS", siteUrl:"https://univelcity-lms.herokuapp.com/", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510850324/screencapture-univelcity-lms-herokuapp-com-index-1510850269515_i8v6b5.png"},
-             
-            {name: "Niger Delta Super Star", siteUrl: "http://nigerdeltasuperstar.org.ng", imageurl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510848302/screencapture-nigerdeltasuperstar-org-ng-1510848266383_ny219p.png"},
-             
-            {name: "Ikonfess", siteUrl: "http://www.ikonfess.com/", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847768/screencapture-www-ikonfess-com-1510847735636_hfrya9.png"},
-             
             {name: "UK pubmed", siteUrl: "http://ukpubmed.org", imageUrl: "http://res.cloudinary.com/dsxfchez8/image/upload/v1510847524/ukpubmed_lo1j9y.png"},
              
         ],
